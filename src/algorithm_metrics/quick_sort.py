d7e1dcd22b57bc2dfba255af55ae9bb2069fcf71
keepa <keepa6421@gmail.com>
2026-06-13 17:50:30 -0400

    added sorting

---
+++ src/algorithm_metrics/quick_sort.py
@@ -0,0 +1,26 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quick_sort(arr):
+    _quick_sort(arr, 0, len(arr) - 1)
+    return arr
+
+
+def _quick_sort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        _quick_sort(arr, low, pivot_index - 1)
+        _quick_sort(arr, pivot_index + 1, high)
+        
+arr = [5,2,9,1,3]
+print(quick_sort(arr))
